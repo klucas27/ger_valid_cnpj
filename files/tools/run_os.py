ab90267379dd71c1d3f98e1394102137dc2572fd
Kresley Lucas <lucas27ro@gmail.com>
2021-12-27 23:05:54 -0300

    00

---
+++ files/tools/run_os.py
@@ -0,0 +1,9 @@
+import os
+
+
+def os_run(cmds):
+    return os.system(f"{cmds}")
+
+
+if __name__ == "__main__":
+    os_run("cls")
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# gerador e validador CNPJ;
+from files.tools import run_os
+
+clear = run_os
+
+def menu():
+    lista_menu = {
+        1: ["Validar CNPJ;"],
+        2: ["Gerar CNPJ"],
+    }
+
+    for nm, vlr in lista_menu.items():
+        print(f"{nm} --> {vlr[0]}")
+
+
+if __name__ == "__main__":
+    menu()
